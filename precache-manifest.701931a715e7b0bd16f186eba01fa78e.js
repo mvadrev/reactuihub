4100a8a0b632ed53d3e25d73f656b43608b13e67
root <mvadrev@gmail.com>
2020-09-04 19:10:54 +0530

    first commit

---
+++ precache-manifest.701931a715e7b0bd16f186eba01fa78e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b50889aef2fc900b826fb61031cf45f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2780883fbab6d48d0bb9",
+    "url": "/static/css/2.0f801562.chunk.css"
+  },
+  {
+    "revision": "6e8aa1223ede22d4d7e8",
+    "url": "/static/css/main.4166e16d.chunk.css"
+  },
+  {
+    "revision": "2780883fbab6d48d0bb9",
+    "url": "/static/js/2.bd5d323a.chunk.js"
+  },
+  {
+    "revision": "c8336e0bcbf8ea1670aafb15aa044bf7",
+    "url": "/static/js/2.bd5d323a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e8aa1223ede22d4d7e8",
+    "url": "/static/js/main.14e3aaf1.chunk.js"
+  },
+  {
+    "revision": "a0ffdb28d03c8ab8b574",
+    "url": "/static/js/runtime-main.a20f385d.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
